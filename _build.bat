d181cb2fb48245e05fa9db85b7ad9a597498f1a0
Scott Chao <zws061003@gmail.com>
2026-05-20 10:35:25 +0800

    chore: 移除误添加的 .bat 文件

--- _build.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 >/dev/null 2>&1
-cd /d "C:\Users\ALIENWARE\Desktop\QT\Smart-Markdown\build\Desktop_Qt_6_11_0_MSVC2022_64bit-Debug"
-nmake -f Makefile.Debug > build_out.txt 2>&1
